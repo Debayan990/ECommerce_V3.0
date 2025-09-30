020c3259c744e8ecbd65c4cac399b7f6041809d5
Debayan Mukherjee <121631241+Debayan990@users.noreply.github.com>
2025-09-30 20:26:35 +0530

    Add files via upload

---
+++ inventory-service/src/main/java/com/cts/service/InventoryService.java
@@ -0,0 +1,17 @@
+package com.cts.service;
+
+import com.cts.dtos.InventoryDto;
+
+import java.util.List;
+
+public interface InventoryService {
+    InventoryDto addInventory(InventoryDto inventoryDto, String token);
+    InventoryDto getInventoryById(Long id);
+    InventoryDto getInventoryByItemId(Long itemId);
+    List<InventoryDto> getAllInventory();
+    InventoryDto updateInventory(Long id, InventoryDto inventoryDto, String token);
+    String deleteInventory(Long id, String token);
+    String deleteInventoryByItemId(Long itemId, String token);
+
+
+}
